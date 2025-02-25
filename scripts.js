6f2941a1c0e94fccc3fce3d346444a0a34ff59b0
Vitor Gallucci <vitorbg04@gmail.com>
2025-02-25 16:12:21 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const uploadBtn = document.getElementById('upload-btn');
+const inputUpload = document.getElementById('image-upload');
+
+uploadBtn.addEventListener('click', () => {
+  inputUpload.click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
